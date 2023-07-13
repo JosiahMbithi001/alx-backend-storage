3e2d5c7f5cdea15513cd56828c8c88676d250d1e
JosiahMbithi001 <mbithishucks@gmail.com>
2023-07-13 00:08:28 -0700

    Task 4 Oders Trigger

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,8 +3,6 @@
 CREATE TRIGGER decrease_quantity_trigger
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE id = NEW.item_id;
-END;
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
