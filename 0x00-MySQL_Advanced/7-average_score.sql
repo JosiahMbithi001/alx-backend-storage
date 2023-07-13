d731adb00418163091ba773377eeb47889a75355
JosiahMbithi001 <mbithishucks@gmail.com>
2023-07-13 01:36:22 -0700

    Corection on Task 7 to a simler method

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,21 +6,13 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
 BEGIN
-    DECLARE total_score  FLOAT;
-    DECLARE total_count INT;
     DECLARE average_score FLOAT;
 
-    -- Compute Total Score
-    SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
-
-    -- Compute total Count
-    SELECT COUNT(*) INTO total_count FROM corrections WHERE user_id = user_id;
-
     -- Compute Average Score
-    SET average_score = IF(total_count > 0, total_score /  total_count, 0);
+    SET average_score = (SELECT AVG(score) FROM corrections AS D WHERE D.user_id=user_id);
 
     -- Update Average Score in users table
-    UPDATE  users SET average_score = average_score WHERE id = user_id;
+    UPDATE  users SET average_score = average_score WHERE id=user_id;
     
 END $$
 
