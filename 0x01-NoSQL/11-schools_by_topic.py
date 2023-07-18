d898efb5cb205dd21a6c39990d3e3aab9bc70524
JosiahMbithi001 <mbithishucks@gmail.com>
2023-07-18 02:53:25 -0700

    Autopep Task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """This File has A Function school_by_topics
 """
+
+
 def schools_by_topic(mongo_collection, topic):
-	"""This Function returns List of Schools with a specific topic"""
-	
-	schools = mongo_collection.find({"topics" : topic})
-	return list(schools)
+    """This Function returns List of Schools with a specific topic"""
+
+    schools = mongo_collection.find({"topics": topic})
+    return list(schools)
